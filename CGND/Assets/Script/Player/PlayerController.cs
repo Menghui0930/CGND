59a91fbcddfcc4f56d623c7fa4d5e0a56210340e
Menghui0930 <chinmh30@gmail.com>
2026-03-29 17:34:47 +0800

    Revert "Small Update #3"
    This reverts commit 540019ed7c97e17af5f949a04bd7acbc5601dbf1.

--- CGND/Assets/Script/Player/PlayerController.cs
+++ CGND/Assets/Script/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
     public static PlayerController instance;
@@ -16,8 +15,8 @@ public class PlayerController : MonoBehaviour {
     [Header("Face Direction")]
     // Face Direction
     public bool facingRight;
-    //private int internalFaceDirection = 1;
-    //private int faceDirection;
+    private int internalFaceDirection = 1;
+    private int faceDirection;
 
     [Header("Jumping")]
     // Jumping
@@ -67,7 +66,6 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void FaceDirection() {
-        /*
         faceDirection = internalFaceDirection;
         facingRight = faceDirection == 1;
 
@@ -79,10 +77,6 @@ public class PlayerController : MonoBehaviour {
             facingRight = false;
         }
         internalFaceDirection = faceDirection;
-        */
-
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        facingRight = mouseWorldPos.x > transform.position.x;
     }
 
     private void RotateModel() {
@@ -92,9 +86,9 @@ public class PlayerController : MonoBehaviour {
     private void OnDrawGizmos() {
         if (groundChecker == null) return;
 
-        // if ground will red color 
+        // 根據 isGrounded 變換顏色
         Gizmos.color = isGrounded ? Color.green : Color.red;
-        Gizmos.DrawWireSphere(groundChecker.position, 0.3f); 
+        Gizmos.DrawWireSphere(groundChecker.position, 0.3f); // 跟你的半徑一樣
     }
 
 
