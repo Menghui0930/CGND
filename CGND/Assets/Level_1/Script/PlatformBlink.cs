1f894f6989624203ce076dd749821be770660adb
Xin Yi <103635699+Xinyi0917@users.noreply.github.com>
2026-04-05 23:31:01 +0800

    Level 1
    - platform 1
    - vine
    - blink platform

---
+++ CGND/Assets/Level_1/Script/PlatformBlink.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlatformBlink : MonoBehaviour
+{
+    [Header("Settings")]
+    public float visibleTime = 5f;    
+    public float invisibleTime = 2f;
+
+    private TilemapRenderer tr;      
+    private TilemapCollider2D col;
+    private float timer;
+    private bool isVisible = true;
+
+    void Start()
+    {
+        tr = GetComponent<TilemapRenderer>();
+        col = GetComponent<TilemapCollider2D>();
+        timer = visibleTime;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            if (isVisible)
+            {
+                isVisible = false;
+                tr.enabled = false;
+                col.enabled = false;
+                timer = invisibleTime;
+            }
+            else
+            {
+                isVisible = true;
+                tr.enabled = true;
+                col.enabled = true;
+                timer = visibleTime;
+            }
+        }
+    }
+}
---
+++ CGND/Assets/Level_1/Script/WaterScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerMotor playerMotor = collision.gameObject.GetComponentInParent<PlayerMotor>();
+            if (playerMotor != null)
+            {
+                Health.OnDeath?.Invoke(playerMotor);
+            }
+        }
+    }
+}
