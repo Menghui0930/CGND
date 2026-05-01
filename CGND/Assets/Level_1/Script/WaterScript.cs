dc8f8b75b9e0c0bbca101c520391416c89f3a030
Xin Yi <103635699+Xinyi0917@users.noreply.github.com>
2026-05-02 00:20:19 +0800

    level 1
    -redo water script

--- CGND/Assets/Level_1/Script/WaterScript.cs
+++ CGND/Assets/Level_1/Script/WaterScript.cs
@@ -2,15 +2,33 @@
 
 public class WaterScript : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision) 
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !triggered)
         {
+            triggered = true;
+
+            Health playerHealth = collision.gameObject.GetComponentInParent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.LoseLife();
+            }
+
             PlayerMotor playerMotor = collision.gameObject.GetComponentInParent<PlayerMotor>();
             if (playerMotor != null)
             {
                 Health.OnDeath?.Invoke(playerMotor);
             }
+            // 1秒后重置
+            Invoke("ResetTrigger", 1f);
         }
+    }
+
+    private void ResetTrigger()
+    {
+        triggered = false;
     }
 }
