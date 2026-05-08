8a2a474d9b5a309f0ac698fc118f55a085860992
Xin Yi <103635699+Xinyi0917@users.noreply.github.com>
2026-05-08 22:25:18 +0800

    level select
    -3level button
    -back button

---
+++ CGND/Assets/Level_Select/LevelSelect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
